1f39d26433026d0645123b7d0b1ce0e39f87a044
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-10 17:00:09 +0300

    Models || AutoCompleteTrie.js || Creating addWord method for AutoCompleteTrie class

--- Models/AutoCompleteTrie.js
+++ Models/AutoCompleteTrie.js
@@ -4,4 +4,28 @@ class AutoCompleteTrie {
     this.children = {};
     this.endOfWord = false;
   }
+  /**
+   * Adding new word to the AutoCompleteTrie
+   * @param {String} word  - words to add to trie.
+   */
+  addWord(word) {
+    if (typeof word !== "string") {
+      //# Invalid Input
+    }
+    if (word.length === 0) {
+      //# Empty String
+    }
+    const letters = word.toLowerCase().split("");
+    let current = this;
+
+    for (const letter of letters) {
+      if (Object.keys(current.children).includes(letter) === false) {
+        current.children[letter] = new AutoCompleteTrie(letter);
+        current = current.children[letter];
+      } else {
+        current = current.children[letter];
+      }
+    }
+    current.endOfWord = true;
+  }
 }
