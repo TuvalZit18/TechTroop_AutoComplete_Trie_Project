0666d0971acb88287951784c98efbe3e684d87f9
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-16 14:52:03 +0300

    Views || AutoCompleteBrowser || Adding empty word message"

--- Views/AutoCompleteBrowser/main.js
+++ Views/AutoCompleteBrowser/main.js
@@ -1,5 +1,7 @@
 const printAddWordMessage = (word) => {
-  return `✓ Added '${word}' to dictionary.`;
+  return word.length > 0
+    ? `✓ Added '${word}' to dictionary.`
+    : `✗ Cannot add empty word`;
 };
 
 const printFoundWordMessage = (word, isWordFound) => {
